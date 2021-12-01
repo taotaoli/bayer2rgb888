11b1b5d4708a17900417d404857a58a06e21b0e5
taotaoli-lt01 <ltt124356871@126.com>
2021-12-01 15:32:26 +0800

    update test bat

--- test_run.bat
+++ test_run.bat
@@ -4,8 +4,8 @@ del /f /q /a "*.tif"
 for %%I in (NEAREST SIMPLE BILINEAR HQLINEAR DOWNSAMPLE VNG AHD ORI) ^
 do (
 ::raw8
-	.\bayer2rgb_C_release.exe -i camera_1600x1200_20080101000000_x_4.00.raw -o camera_1600x1200_20080101000000_x_4.00.%%I.tif -w 1600 -v 1200 -b 8 -f RGGB -m %%I -t
+	.\bayer2rgb888_C_release.exe -i camera_1600x1200_20080101000000_x_4.00.raw -o camera_1600x1200_20080101000000_x_4.00.%%I.tif -w 1600 -v 1200 -b 8 -f RGGB -m %%I -t
 ::raw12
-	.\bayer2rgb_C_release.exe -i BGGR_2560x1440_raw12_unpack.raw -o BGGR_2560x1440_raw12_unpack.%%I.tif -w 2560 -v 1440 -b 12 -f BGGR -m %%I -t
+	.\bayer2rgb888_C_release.exe -i BGGR_2560x1440_raw12_unpack.raw -o BGGR_2560x1440_raw12_unpack.%%I.tif -w 2560 -v 1440 -b 12 -f BGGR -m %%I -t
 )
 
